ec4fc344b7feba548cb6d3371df87b2f26afa277
JayarathnaR <jayarathnar42@gmail.com>
2026-07-03 15:37:56 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector <vector<string>> ans;
+        unordered_map<string,vector<string>> sortedStrs;
+        for(int i =0 ; i<strs.size();i++){
+            string key = strs[i];
+            sort(key.begin(), key.end());
+            sortedStrs[key].push_back(strs[i]);
+            
+        }
+        for(auto it:sortedStrs){
+            ans.push_back(it.second);
+        }
+        return ans;
+
+    }
+};
