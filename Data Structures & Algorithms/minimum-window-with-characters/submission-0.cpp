c60e074aba3319f1e08a1c33a8ce7fdebf8b631c
JayarathnaR <jayarathnar42@gmail.com>
2026-09-03 11:57:05 +0530

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+
+        if(t.size()>s.size()){
+            return "";
+        }
+        unordered_map<char,int>need ;
+        unordered_map<char,int>window ;
+        
+
+        for(char c : t){
+            need[c]++;
+        }
+
+        int left =0;
+        int start =0;
+        int have =0;
+        int required = need.size();
+        int minLen = INT_MAX;
+
+        for(int right =0 ; right<s.size();right++){
+            char c = s[right];
+            window[c]++;
+
+            if(need.count(c) && window[c]==need[c]){
+                have++;
+            }
+
+            while(have == required){
+                if(right-left+1 < minLen){
+                    minLen = right - left+1;
+                    start = left;
+                }
+                char leftChar = s[left];
+                window[leftChar]--;
+
+                if(need.count(leftChar) && window[leftChar]<need[leftChar]){
+                    have--;
+                }
+                left++;
+            }
+
+        }
+        if(minLen == INT_MAX){
+            return "";
+        }
+
+        return s.substr(start,minLen);
+    }
+};
