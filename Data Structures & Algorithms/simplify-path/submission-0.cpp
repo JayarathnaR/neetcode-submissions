d40d119c5f49a5bcfdc40c4255f2bc4b0133504c
JayarathnaR <jayarathnar42@gmail.com>
2026-09-17 11:26:13 +0530

    Add: simplify-path - submission-0

---
+++ Data Structures & Algorithms/simplify-path/submission-0.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack <string> st;
+
+        stringstream ss(path);
+        string part;
+
+        while(getline(ss , part ,'/')){
+            if(part =="" || part =="."){
+                continue;
+            }
+
+            else if(part == ".."){
+                if(!st.empty()){
+                    st.pop();
+                }
+            }
+            else{
+                st.push(part);
+            }
+        }
+
+        string ans = "";
+        while(!st.empty()){
+            ans = "/" + st.top()+ans;
+            st.pop();
+        }
+        if(ans == ""){
+            return "/";
+        }
+        return ans;
+    }
+};
